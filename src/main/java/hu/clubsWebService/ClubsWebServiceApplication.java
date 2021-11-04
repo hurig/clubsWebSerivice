82515bb96bfb530fdc143c83260d14d808597acc
hurig <hurig@nyirszikszi.hu>
2021-11-04 14:30:17 +0100

    empty version

---
+++ src/main/java/hu/clubsWebService/ClubsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package hu.clubsWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubsWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/clubsWebService/ClubsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.clubsWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubsWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
